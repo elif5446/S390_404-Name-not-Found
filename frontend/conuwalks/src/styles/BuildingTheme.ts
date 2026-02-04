2bcd73fe73aae90fe358086b24025c3852143e46
Jennifer <jennyavakian@hotmail.com>
2026-02-04 18:29:04 -0500

    Updated the store building information #60

---
+++ frontend/conuwalks/src/styles/BuildingTheme.ts
@@ -0,0 +1,25 @@
+export const BuildingTheme = {
+  SGW: {
+    H: "#0A84FF",
+    LB: "#34C759",
+    LS: "#AF52DE",
+    EV: "#FF9F0A",
+    MB: "#30D158",
+    FB: "#FFD60A",
+    FG: "#64D2FF",
+    CL: "#FF375F",
+    GM: "#FF375F",
+    ER: "#FFCC00"
+  },
+  LOY: {
+    VL: "#0A84FF",
+    SP: "#34C759",
+    CJ: "#AF52DE",
+    CC: "#FF9F0A",
+    AD: "#30D158",
+    FC: "#FFD60A",
+    HU: "#64D2FF"
+  }
+};
+
+export default BuildingTheme;
