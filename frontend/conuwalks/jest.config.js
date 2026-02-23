0fb80852e79743c4021ffe5e6ba505a8cc4b1cdd
Yasmeen Adi <yasmin-adi@hotmail.com>
2026-02-23 17:07:56 -0500

    Revert "Unit tests "

--- frontend/conuwalks/jest.config.js
+++ frontend/conuwalks/jest.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  preset: 'jest-expo',
-  testEnvironment: 'jsdom',
-  setupFiles: ['<rootDir>/jest.setup.js'],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  preset: "jest-expo",
+  setupFiles: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '@react-native-async-storage/async-storage': require.resolve(
-      '@react-native-async-storage/async-storage/jest/async-storage-mock'
-    ),
-    '@react-native-google-signin/google-signin': '<rootDir>/jest.setup.js',
+    "^@/(.*)$": "<rootDir>/$1",
   },
 };
