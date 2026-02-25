e19ce2638bdf53e54c745710111ba3c3a13fbb12
Kristi Doce <kristidoce@gmail.com>
2026-02-25 18:50:35 -0500

    add update tests and dependencies

--- frontend/conuwalks/app.config.js
+++ frontend/conuwalks/app.config.js
@@ -1,53 +1,55 @@
 export default {
-  "expo": {
-    "name": "conuwalks",
-    "slug": "conuwalks",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "scheme": "conuwalks",
-    "userInterfaceStyle": "automatic",
-    "newArchEnabled": true,
-    "ios": {
-      "supportsTablet": true,
-      "infoPlist": {
-        "NSLocationWhenInUseUsageDescription": "conuwalks needs your location to show you on the map."
+  expo: {
+    name: "conuwalks",
+    slug: "conuwalks",
+    version: "1.0.0",
+    orientation: "portrait",
+    scheme: "conuwalks",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      infoPlist: {
+        NSLocationWhenInUseUsageDescription:
+          "conuwalks needs your location to show you on the map.",
       },
-      "bundleIdentifier": "com.conuwalks.app"
+      bundleIdentifier: "com.conuwalks.app",
     },
-    "android": {
-      "edgeToEdgeEnabled": true,
-      "predictiveBackGestureEnabled": false,
-      "package": "com.anonymous.conuwalks",
-      "config": {
-        "googleMaps": {
-          "apiKey": process.env.GOOGLE_MAPS_API_KEY
-        }
+    android: {
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      package: "com.anonymous.conuwalks",
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY,
+        },
       },
-      "permissions": ["ACCESS_COARSE_LOCATION", "ACCESS_FINE_LOCATION"]
+      permissions: ["ACCESS_COARSE_LOCATION", "ACCESS_FINE_LOCATION"],
     },
-    "web": {
-      "output": "static",
-      "favicon": "./assets/images/favicon.png"
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png",
     },
-    "plugins": [
+    plugins: [
+      "@react-native-google-signin/google-signin",
       "expo-router",
       [
         "expo-splash-screen",
         {
-          "image": "./assets/images/splash-icon.png",
-          "imageWidth": 200,
-          "resizeMode": "contain",
-          "backgroundColor": "#ffffff",
-          "dark": {
-            "backgroundColor": "#000000"
-          }
-        }
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000",
+          },
+        },
       ],
-      "./withMapRenderer.js"
+      "./withMapRenderer.js",
     ],
-    "experiments": {
-      "typedRoutes": true,
-      "reactCompiler": true
-    }
-  }
-}
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+  },
+};
