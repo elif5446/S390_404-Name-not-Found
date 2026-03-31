aabe55d53fa3eac0bfd5a722faa097c5e47beb19
yas176 <yasmin-adi@hotmail.com>
2026-03-31 15:57:32 -0400

    Fixing issues for Sonar

--- frontend/conuwalks/src/_tests_/unit/__mocks__/@expo/vector-icons.js
+++ frontend/conuwalks/src/_tests_/unit/__mocks__/@expo/vector-icons.js
@@ -1,3 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 export const Ionicons = (props) => <>{props.children}</>;
+Ionicons.propTypes = {
+	children: PropTypes.node
+};
+
 export const MaterialIcons = (props) => <>{props.children}</>;
+MaterialIcons.propTypes = {
+	children: PropTypes.node
+};
